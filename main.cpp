e337f6ad454bf15c58c4d64ed225c3084f56faea
mkien14 <kien14032022@gmail.com>
2025-05-12 11:51:46 +0700

    wdwd
    dwd

--- main.cpp
+++ main.cpp
@@ -1,9 +1,40 @@
 #include <iostream>
+#include <SDL.h>
+#include <SDL_image.h>
 
 using namespace std;
 
-int main()
+int main(int argc, char *argv[])
 {
-    cout << "Hello world!" << endl;
+    SDL_Init(SDL_INIT_VIDEO);
+    IMG_Init(IMG_INIT_PNG);
+
+    SDL_Window* window = SDL_CreateWindow("SDL2 Image Loading", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_SHOWN);
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+    SDL_Surface* imageSurface = IMG_Load("cell_o.png");
+    SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, imageSurface);
+    SDL_FreeSurface(imageSurface);
+
+    bool running = true;
+    SDL_Event event;
+    while (running) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                running = false;
+            }
+        }
+
+        SDL_RenderClear(renderer);
+        SDL_RenderCopy(renderer, texture, NULL, NULL);
+        SDL_RenderPresent(renderer);
+    }
+
+    SDL_DestroyTexture(texture);
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    IMG_Quit();
+    SDL_Quit();
+
     return 0;
 }
